dea0f5cffed58c6a37a513376493fce95129241d
JezzVega <jesusvegamoreno@gmail.com>
2019-04-30 16:32:34 -0500

    Categorias Dinamicas

--- Js/navegacion.js
+++ Js/navegacion.js
@@ -6,6 +6,7 @@ $('.link').on('click', function(){
 
 		case "#home": 
 			$('.carrito').css('display', 'none');
+			$('.categorias').css('display', 'none');
 			$('.admin').css('display', 'none');
 			$('.login').css('display', 'none');
 			$('.home').css('display', 'block');
@@ -13,6 +14,7 @@ $('.link').on('click', function(){
 
 		case "#admin": 
 			$('.carrito').css('display', 'none');
+			$('.categorias').css('display', 'none');
 			$('.home').css('display', 'none');
 			$('.login').css('display', 'none');
 			$('.admin').css('display', 'block');
@@ -20,11 +22,31 @@ $('.link').on('click', function(){
 
 		case "#carrito": 
 			$('.admin').css('display', 'none');
+			$('.categorias').css('display', 'none');
 			$('.home').css('display', 'none');
 			$('.login').css('display', 'none');
 			$('.carrito').css('display', 'block');
 		break;
 
+	}
+
+});
+
+$('.categList').on('click', '.link', function(){
+
+	var link = $(this).attr('href');
+	var nombCateg = $(this).attr('nombCateg');
+
+	if(link == "#categorias"){
+		
+		$('.carrito').css('display', 'none');
+		$('.admin').css('display', 'none');
+		$('.login').css('display', 'none');
+		$('.home').css('display', 'none');
+		$('.categorias').css('display', 'block');
+
+		$('.categTitle').empty();
+		$('.categTitle').append('<b>'+nombCateg+'</b>');
 	}
 
 });
